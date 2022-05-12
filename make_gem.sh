1d3691cbb4d3b76fb83aea1920ae082cb24acc1c
Jorge Novo <jnovos@users.noreply.github.com>
2022-05-12 11:53:43 +0200

    Update

--- make_gem.sh
+++ make_gem.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-if [ -f jekyll_lunrjs-1.0.0.gem ]; then
+if [ -f jekyll_lunrjs-1.0.1.gem ]; then
     echo "Delete File!"
-    rm jekyll_lunrjs-1.0.0.gem
+    rm jekyll_lunrjs-1.0.1.gem
     gem uninstall jekyll_lunrjs
 fi
 gem build jekyll_lunrjs.gemspec
-gem install jekyll_lunrjs-1.0.0.gem --local
+gem install jekyll_lunrjs-1.0.1.gem --local
